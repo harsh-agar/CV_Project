3bdc51918ec1f013ac2a7a667080252cd67adcd5
Harsh Agarwal <harsh.agar97@gmail.com>
2022-02-27 12:46:23 +0100

    implemented sample weighing

--- Task1_pseudoLabeling/main.py
+++ Task1_pseudoLabeling/main.py
@@ -34,13 +34,13 @@ def main(args):
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    if not os.path.exists(Path(args.datapath) / Path('dataloaders')):
-        os.makedirs(Path(args.datapath) / Path('dataloaders'))
+    if not os.path.exists(Path(args.datapath) / Path('dataloaders_%s' %(args.dataset))):
+        os.makedirs(Path(args.datapath) / Path('dataloaders_%s' %(args.dataset)))
 
-    labeled_loader_path = Path(args.datapath) / Path('dataloaders') / Path('labeled_loader.pkl')
-    valid_loader_path = Path(args.datapath) / Path('dataloaders') / Path('valid_loader.pkl')
-    unlabeled_loader_path = Path(args.datapath) / Path('dataloaders') / Path('unlabeled_loader.pkl')
-    test_loader_path = Path(args.datapath) / Path('dataloaders') / Path('test_loader.pkl')
+    labeled_loader_path = Path(args.datapath) / Path('dataloaders_%s' %(args.dataset)) / Path('labeled_loader.pkl')
+    valid_loader_path = Path(args.datapath) / Path('dataloaders_%s' %(args.dataset)) / Path('valid_loader.pkl')
+    unlabeled_loader_path = Path(args.datapath) / Path('dataloaders_%s' %(args.dataset)) / Path('unlabeled_loader.pkl')
+    test_loader_path = Path(args.datapath) / Path('dataloaders_%s' %(args.dataset)) / Path('test_loader.pkl')
 
     if os.path.exists(labeled_loader_path) and os.path.exists(valid_loader_path) and os.path.exists(unlabeled_loader_path) and os.path.exists(test_loader_path):
         labeled_dataset = torch.load(labeled_loader_path)
@@ -51,7 +51,7 @@ def main(args):
     else:
         val_set_idx = np.empty(0)
         for i in set(unlabeled_dataset.targets):
-            data_size = int(np.where(unlabeled_dataset.targets ==  0)[0].shape[0]*0.1)
+            data_size = int(np.where(unlabeled_dataset.targets ==  i)[0].shape[0] * 0.1)
             val_set_idx = np.append(val_set_idx, np.random.choice(np.where(unlabeled_dataset.targets ==  i)[0], data_size))
 
         x_val = torch.empty(0, 3, 32, 32)
@@ -61,12 +61,12 @@ def main(args):
         for i in tqdm(range(len(unlabeled_dataset))):
             if i in val_set_idx:
                 x_val = torch.cat((x_val, unlabeled_dataset[i][0][None, :]))
-                y_val = torch.cat((y_val, torch.tensor(unlabeled_dataset.targets[i : i+1])))            
+                y_val = torch.cat((y_val, torch.tensor(unlabeled_dataset.targets[i : i+1])))
             else:
                 x_unl = torch.cat((x_unl, unlabeled_dataset[i][0][None, :]))
                 y_unl = torch.cat((y_unl, torch.tensor(unlabeled_dataset.targets[i : i+1])))
-        unlabeled_dataset_split = list(zip(x_val, y_val))
-        valid_dataset = list(zip(x_unl, y_unl))
+        valid_dataset = list(zip(x_val, y_val))
+        unlabeled_dataset_split = list(zip(x_unl, y_unl))
     
         torch.save(labeled_dataset, labeled_loader_path)
         torch.save(unlabeled_dataset_split, unlabeled_loader_path)
@@ -98,9 +98,8 @@ def main(args):
     # TODO: SUPPLY your code
     ############################################################################
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.wd, momentum=args.momentum)
 
-    curr_use_data = 'labelled'
     model_wt_path = Path('model_weights_%s_%.2f' %(args.dataset, args.threshold)) 
     model_txt_path  = Path(model_wt_path) / Path("epoch_info.txt" )
     model_last_path = Path(model_wt_path) / Path("last_trained.h5")
@@ -132,8 +131,9 @@ def main(args):
                 o_ul = model(x_ul)
                 x_ul = x_ul.to('cpu')
                 o_ul = o_ul.to('cpu')
-                x_t = torch.cat((x_t, x_ul[torch.where(torch.max(o_ul.softmax(dim=1), axis=1)[0] > 0.95)[0]]))
-                y_t = torch.cat((y_t, o_ul.softmax(dim=1).max(dim=1)[1][torch.where(torch.max(o_ul.softmax(dim=1), axis=1)[0] > 0.95)[0]]))
+                x_t = torch.cat((x_t, x_ul[torch.where(torch.max(o_ul.softmax(dim=1), axis=1)[0] > args.threshold)[0]]))
+                y_t = torch.cat((y_t, o_ul.softmax(dim=1).max(dim=1)[1][torch.where(torch.max(o_ul.softmax(dim=1), axis=1)[0] > args.threshold)[0]]))
+                w_t = torch.sigmoid(torch.max(o_ul.softmax(dim=1), axis=1)[0])*2 - 1
         print(x_t.shape[0])
         train_accuracies = epoch_log()
         val_accuracies   = epoch_log()
@@ -141,11 +141,16 @@ def main(args):
         for i in tqdm(range(args.iter_per_epoch)):
 
             try:
-                    x_l, y_l    = next(labeled_loader)
+                    data_sample = next(labeled_loader)
+                    if len(data_sample) == 2:
+                        x_l, y_l      = data_sample
+                        w_l = torch.ones(x_l.shape[0])
+                    else:
+                        x_l, y_l, w_l = data_sample
 
             except StopIteration:
                 if x_t.shape[0] > 0:
-                    labeled_loader      = iter(itertools.chain(DataLoader(list(zip(x_t, y_t)), 
+                    labeled_loader      = iter(itertools.chain(DataLoader(list(zip(x_t, y_t, w_t)), 
                                                                           batch_size = args.train_batch, 
                                                                           shuffle = True, 
                                                                           num_workers=args.num_workers),
@@ -158,14 +163,22 @@ def main(args):
                                                           batch_size = args.train_batch, 
                                                           shuffle = True, 
                                                           num_workers=args.num_workers))
-                x_l, y_l    = next(labeled_loader)
+
+                data_sample = next(labeled_loader)
+                if len(data_sample) == 2:
+                    x_l, y_l      = data_sample
+                    w_l = torch.ones(x_l.shape[0])
+                else:
+                    x_l, y_l, w_l = data_sample
             
-            x_l, y_l    = x_l.to(device), y_l.to(device)
+            x_l, y_l, w_l    = x_l.to(device), y_l.to(device), w_l.to(device)
 
             optimizer.zero_grad()
 
             o_l = model(x_l)
-            loss = criterion(o_l.softmax(dim=1), y_l)
+            criterion_weighted = nn.CrossEntropyLoss(weight=class_wts, reduction='mean')
+            class_wts = torch.ones(args.num_classes).to(device) * torch.mean(w_l)
+            loss = criterion_weighted(o_l.softmax(dim=1), y_l)
             loss.backward()
             optimizer.step()
 
@@ -173,7 +186,7 @@ def main(args):
 
             train_accuracies.update(train_acc[0].item(), x_l.shape[0])
 
-            running_loss_train += loss.item()
+            running_loss_train += loss.mean().item()
 
         val_loss = 0.0
         for val_i, data in enumerate(valid_loader, 0):
@@ -227,13 +240,6 @@ def main(args):
             # TODO: SUPPLY your code
             ####################################################################
 
-
-
-
-
-
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pseudo labeling \
                                         of CIFAR10/100 with pytorch")
@@ -247,7 +253,7 @@ if __name__ == "__main__":
                         help="The initial learning rate") 
     parser.add_argument("--momentum", default=0.9, type=float,
                         help="Optimizer momentum")
-    parser.add_argument("--wd", default=0, type=float,
+    parser.add_argument("--wd", default=0.00, type=float,
                         help="Weight decay")
     parser.add_argument("--expand-labels", action="store_true", 
                         help="expand labels to fit eval steps")
@@ -255,9 +261,9 @@ if __name__ == "__main__":
                         help='train batchsize')
     parser.add_argument('--test-batch', default=256, type=int,
                         help='train batchsize')
-    parser.add_argument('--total-iter', default=128*256, type=int,
+    parser.add_argument('--total-iter', default=256*256, type=int,
                         help='total number of iterations to run')
-    parser.add_argument('--iter-per-epoch', default=128, type=int,
+    parser.add_argument('--iter-per-epoch', default=256, type=int,
                         help="Number of iterations to run per epoch")
     parser.add_argument('--num-workers', default=0, type=int, #4
                         help="Number of workers to launch during training")
