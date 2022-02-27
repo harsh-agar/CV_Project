af0d459ae1b26dc425dbf0c537f776195bc3cd0f
Zurana Mehrin Ruhi <zuru00001@uni-saarland.de>
2022-02-27 14:32:21 +0100

    vat and metrics update

--- Task2_VAT/vat.py
+++ Task2_VAT/vat.py
@@ -2,6 +2,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import os
+from pathlib import Path
 
 def l2_norm(r):
     r /= torch.norm(r, dim=1, keepdim=True) + 1e-8
@@ -14,6 +16,7 @@ class VATLoss(nn.Module):
         self.xi = args.vat_xi
         self.eps = args.vat_eps
         self.vat_iter = args.vat_iter
+        self.imagepath = args.datapath / Path('vat_images')
 
     def forward(self, model, x):
 
@@ -21,11 +24,11 @@ class VATLoss(nn.Module):
             preds = F.softmax(model(x), dim=1)
 
         #random tensor sample 
-        r = torch.rand(x.shape).sub(0.5).to(x.device)
+        r = torch.randn(x.shape).to(x.device)
         r = l2_norm(r)
  
         for _ in range(self.vat_iter): 
-            r.requires_grad_()                                                      #torch function why isn't it available??????? 
+            r.requires_grad_()                                                      
             advEx = x + self.xi * r
             advPred = F.log_softmax(model(advEx), dim=1)
             adv_dist = F.kl_div(advPred, preds, reduction='batchmean')
@@ -33,6 +36,16 @@ class VATLoss(nn.Module):
             #storing the gradient of advDistance in d 
             d = l2_norm(r.grad)
             model.zero_grad()
+        
+        if not os.path.exists(self.imagepath):
+            os.makedirs(self.imagepath)
+        
+        #saving adversarial examples 
+        # for i, data in enumerate(advEx): 
+        #     torch.save(data[0], self.imagepath / Path('adversarial_img{}'.format(i)))
+        #     if i <=20: 
+        #         break;
+          
 
         #calculating the adversarial pertubation 
         r_adv = d * self.eps
